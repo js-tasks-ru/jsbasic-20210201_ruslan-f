b0ca54da129aafdd962afd84feb26d6d4126006b
Ruslan Fattyahov <rfattyahov@gmail.com>
2021-02-10 10:50:30 +0300

    Added 2 module task 1

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -5,4 +5,11 @@
  */
 function sumSalary(salaries) {
   // ваш код...
+  let result = 0;
+  for (let key in salaries) {
+    if (isFinite(salaries[key]) ) {
+      result += salaries[key];
+    }
+  }
+  return result;
 }
