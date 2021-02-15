59c3a9b3523bd94e673d1f8589867c361857a7d0
Ruslan Fattyahov <rfattyahov@gmail.com>
2021-02-15 10:43:29 +0300

    Added 3 modul task 4

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,12 @@
 function showSalary(users, age) {
   // ваш код...
+  let result = [];
+  
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].age <= age) {
+      result.push(`${users[i].name}, ${users[i].balance}`);
+    }
+  }
+
+  return result.join('\n');
 }
