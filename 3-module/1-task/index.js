b4f23016df1b59e9b82d7168fdb19e2d72bba79b
Ruslan Fattyahov <rfattyahov@gmail.com>
2021-02-16 09:55:39 +0300

    Added 3 module task 5

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -4,9 +4,5 @@
  */
 function namify(users) {
   // ваш код...
-  let arr = [];
-  for (let i = 0; i < users.length; i++) {
-    arr.push(users[i].name);
-  }
-  return arr;
+  return users.map( item => item.name);
 }
