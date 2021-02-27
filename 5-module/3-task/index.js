5eadf97af19f122803af9c554bc8741cac328c8b
Ruslan Fattyahov <rfattyahov@gmail.com>
2021-02-27 12:55:22 +0300

    Added 5 module task 3

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,40 @@
 function initCarousel() {
   // ваш код...
+  let list = document.querySelector('.carousel__inner');
+  let listItems = document.querySelectorAll('.carousel__slide');
+
+  let left = document.querySelector('.carousel__arrow_left');
+  let right = document.querySelector('.carousel__arrow_right');
+
+  let step = 0;
+  
+  checks ()
+  function checks () {
+    let itemWidth = list.offsetWidth * step;
+    list.style.transform = `translateX( ${-itemWidth}px )`;
+
+    if (listItems[step] == listItems[listItems.length -1]) {
+      right.style.display = 'none';
+    } else {
+      right.style.display = '';
+    }
+
+    if (listItems[step] == listItems[0]) {
+      left.style.display = 'none';
+    } else {
+      left.style.display = '';
+    }
+  }
+
+  right.addEventListener('click', function() {
+    step++;
+
+    checks ()
+  })
+
+  left.addEventListener('click', function() {
+    step--;
+
+    checks ()
+  })
 }
