3b321c1284c18bc5939be917cca0a9b78bc02f0c
Ruslan Fattyahov <rfattyahov@gmail.com>
2021-03-01 11:36:56 +0300

    Added 6 module task 1

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,45 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.table = document.createElement('table');
+
+    this.table.innerHTML = `
+      <thead>
+          <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Зарплата</th>
+            <th>Город</th>
+            <th></th>
+          </tr>
+      </thead>`;
+    
+    let row = this.rows.map( rowItem => {
+      return `
+        <tr>
+          <td>${rowItem.name}</td>
+          <td>${rowItem.age}</td>
+          <td>${rowItem.salary}</td>
+          <td>${rowItem.city}</td>
+          <td><button>X</button></td>
+        </tr>
+      `;
+    }).join('');
+
+    this.table.innerHTML += `
+      <tbody>
+        ${row}
+      <tbody>
+    `;
+
+    this.table.addEventListener('click', (event) {
+      if (event.target.tagName != 'BUTTON') {
+        return;
+      }
+  
+      let tr = event.target.closest('tr');
+  
+      tr.remove();
+    });
   }
 }
